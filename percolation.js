0c4eb55180987913bb16cc987220090a73807766
Kyle Clark <kylesheldonclark@gmail.com>
2021-07-14 02:35:58 +1200

    create grid of sites initialized to state blocked

---
+++ percolation.js
@@ -0,0 +1,47 @@
+/*
+
+given a grid of n by n sites
+  each site is either blocked, open, or full
+  a full site is a site that can be connected to an open site in the top row via a chain of neighbouring open sites
+  a system percolates if there is a full site in the bottom row
+
+Problem
+  if sites are independently set to be open with probability p what is the probability that the system percolates
+  when p is 0 the system does not percolate
+  when p is 1 the system percolates
+
+  when n is sufficiently large there is a threshold value p*
+  when p < p* a system almost never percolates
+  when p > p* a system almost always percolates
+  estimate p*
+
+Grid of nodes
+  nodes can have 3 states
+    blocked Site
+    Open Site
+    Full Open Site
+
+  initialize grid of blocked sites
+  open site i
+  check if site is full site
+  check if site is open site
+    keep track of num of open sites
+  check if system percolates
+
+*/
+let grid = [];
+
+function createGrid(n) {
+  let row = 0;
+  for (let i = 0; i < n*n; i++) {
+    grid.push({
+      position: (Math.floor(row)+'-'+i),
+      state: 'blocked'
+    })
+    row += (1/n);
+  }
+}
+
+createGrid(10);
+console.log(grid);
+console.log(grid.length);
